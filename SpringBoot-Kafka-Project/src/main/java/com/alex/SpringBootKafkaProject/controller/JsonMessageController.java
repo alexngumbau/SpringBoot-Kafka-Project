5bfdff1b1315f9c99d5306d79ca849b86d4773f9
Alex Mutinda Ngumbau <ALEX.NGUMBAU@equitybank.co.ke>
2024-02-15 11:08:23 +0300

    Sending and receiving json objects using JsonSerializer and JsonDeserializer in apache kafka---Configurations DONE

---
+++ SpringBoot-Kafka-Project/src/main/java/com/alex/SpringBootKafkaProject/controller/JsonMessageController.java
@@ -0,0 +1,28 @@
+package com.alex.SpringBootKafkaProject.controller;
+
+import com.alex.SpringBootKafkaProject.kafkaConfigs.JsonKafkaProducer;
+import com.alex.SpringBootKafkaProject.payload.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/kafka")
+public class JsonMessageController {
+
+    private JsonKafkaProducer jsonKafkaProducer;
+
+
+    public JsonMessageController(JsonKafkaProducer jsonKafkaProducer) {
+        this.jsonKafkaProducer = jsonKafkaProducer;
+    }
+
+    @PostMapping("/publish2")
+    public ResponseEntity<String> publish(@RequestBody User user) {
+        jsonKafkaProducer.sendMessage(user);
+        return ResponseEntity.ok("Json message sent to kafka topic");
+    }
+
+}
